c13ccfefca2199f4426267e14335ef215f662ae8
trevormunoz <trevor@trevormunoz.com>
2021-04-23 11:59:14 -0400

    Revert this mess

--- lakeland_db_migrate_v4/destinations.py
+++ lakeland_db_migrate_v4/destinations.py
@@ -34,12 +34,10 @@ class MigratedRecord(V4BaseRecord):
     airtable_created_time: InitVar[str]
     airtable_idno: InitVar[str]
     source_record_hash: int = field(init=False)
-    _v3_airtable_idno: str = field(init=False)
 
     def __post_init__(self, airtable_idno: str, airtable_created_time: str) -> None:
         """Create a hash of id, created time for source record."""
         self.source_record_hash = hash((airtable_idno, airtable_created_time))
-        self._v3_airtable_idno = airtable_idno
 
 
 @dataclass
